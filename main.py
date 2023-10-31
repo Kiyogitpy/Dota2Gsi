407fb8f35285ec2d41369cebb2c2575525b0374a
Kiyogitpy <139643727+Kiyogitpy@users.noreply.github.com>
2023-10-31 04:26:44 +0100

    Cleaned up main application

--- main.py
+++ main.py
@@ -1,45 +1,73 @@
-import keyboard
-from subprocess import Popen
-from pygame import mixer, time
-import os
 import json
+import os
+import subprocess
+
+import keyboard
+from pygame import mixer, time
 
 
-current_directory = os.getcwd()
-audio_folder = os.path.join(current_directory, 'Audio')
-audio_file_path = os.path.join(audio_folder, 'goodbye.wav')
+class AudioPlayer:
+    def __init__(self, file_path, volume_percent):
+        self.file_path = file_path
+        self.volume_percent = volume_percent
+        mixer.init()
+        self.load_file()
+
+    def load_file(self):
+        try:
+            mixer.music.load(self.file_path)
+            mixer.music.set_volume(self.volume_percent / 100)
+        except Exception as e:
+            print(f"Error initializing the audio player: {e}")
+
+    def play(self):
+        mixer.music.play()
+        time.wait(2000)  # waits for 2sec
 
 
-mixer.init()
+class ProcessHandler:
+    def __init__(self, python_scripts, shutdown_key):
+        self.processes = []
+        self.shutdown_key = shutdown_key
+        for script in python_scripts:
+            self.spawn_process(script)
 
-try:
+        keyboard.add_hotkey(self.shutdown_key, self.terminate_processes)
+        keyboard.wait()
+
+    def spawn_process(self, script):
+        try:
+            p = subprocess.Popen(['python', script])
+            self.processes.append(p)
+        except Exception as errs:
+            print(f'Error running {script}: {errs}')
+
+    def terminate_processes(self):
+        for process in self.processes:
+            process.terminate()
+
+
+def from_config():
     with open('config.json', 'r') as config_file:
-            config = json.load(config_file)
-            mixer.music.set_volume = config['volume_percent'] / 100
-except (json.JSONDecodeError, KeyError):
-    print("Error reading config.json, using default values")
+        config = json.load(config_file)
+    return config
 
-processes = []
 
-def terminate_processes():
-    for process in processes:
-        process.terminate()
+def main():
+    base_dir = os.getcwd()
+    config = from_config()
+    audio_file = os.path.join(
+        base_dir, 'Audio', config.get('audio_file', 'goodbye.wav'))
+    audio_volume = config.get('volume_percent', 100)
 
-    
-    mixer.music.load(audio_file_path)
-    mixer.music.play()
+    player = AudioPlayer(audio_file, audio_volume)
 
-    
-    time.wait(2000)  # waits for 2sec
+    handler = ProcessHandler(['flask_server.py', 'menu.py'], 'page down')
 
-    
+    player.play()
+
     os._exit(0)
 
 
-keyboard.add_hotkey('page down', terminate_processes)
-
-
-processes.append(Popen(['python', 'flask_server.py']))
-processes.append(Popen(['python', 'menu.py']))
-
-keyboard.wait()
+if __name__ == '__main__':
+    main()
